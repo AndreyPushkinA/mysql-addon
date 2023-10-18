bc3bfc53a659e84a8ffef3f9fb72cfd4b2bd5d89
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-18 21:59:24 +0200

    Update run.sh

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-/usr/bin/clickhouse-server --config-file /etc/clickhouse-server/config.xml &
-
 OPTIONS_PATH="/data/options.json"
 
 get_option_value() {
     grep "\"$1\"" $OPTIONS_PATH | cut -d ':' -f2 | tr -d ' ",'
 }
 
-DB_NAME=$(get_option_value "db_name")
+DB_NAME=$(get_option_value "dab_name")
 PG_HOST=$(get_option_value "pg_host")
 PG_PORT=$(get_option_value "pg_port")
 PG_DB=$(get_option_value "pg_db")
@@ -17,4 +15,4 @@ PG_PASS=$(get_option_value "pg_pass")
 CH_HOST=$(get_option_value "ch_host")
 CH_PORT=$(get_option_value "ch_port")
 
-echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ENGINE = MaterializedPostgreSQL('${PG_HOST}:${PG_PORT}', '${PG_DB}', '${PG_USER}', '${PG_PASS}');" | clickhouse-client --host ${CH_HOST} --port ${CH_PORT}
+clickhouse-server -C /etc/clickhouse-server/config.xml & sleep 3 && echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ENGINE = MaterializedPostgreSQL('${PG_HOST}:${PG_PORT}', '${PG_DB}', '${PG_USER}', '${PG_PASS}');" | clickhouse-client --host ${CH_HOST} --port ${CH_PORT}
